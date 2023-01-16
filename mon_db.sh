d0355d09b1cf85a7685b33bb52d66af167c24155
mutate-github <mutate@mail.ru>
2023-01-16 10:36:01 +0600

    Mon 16 Jan 2023 10:36:00 +06

--- mon_db.sh
+++ mon_db.sh
@@ -16,35 +16,21 @@ for HOST in `echo "$HOSTS" | xargs -n1 echo`; do
   for DB in  `echo "$DBS" | xargs -n1 echo`; do
     echo "DB="$DB
     LOGF=$LOGDIR/mon_db_${HOST}_${DB}.log
-    $WRTPI $HOST $DB db > $LOGF
-# v$instance
-    LOGF_INST_DIFF=$LOGDIR/mon_db_${HOST}_${DB}_inst_diff.log
-    LOGF_INST_OLD=$LOGDIR/mon_db_${HOST}_${DB}_inst_old.log
-    touch $LOGF_INST_OLD
-    LOGF_INST=$LOGDIR/mon_db_${HOST}_${DB}_inst.log
-    cat $LOGF | egrep -A3 -i INSTANCE_NAME | grep -v '\----' > $LOGF_INST
-    diff $LOGF_INST_OLD $LOGF_INST > $LOGF_INST_DIFF
-    rc_inst=$?
+    $WRTPI $HOST $DB db | sed -n '2,12p' | grep -v '\----' | sed '/^$/d' > $LOGF
 
-# v$database
     LOGF_DB_DIFF=$LOGDIR/mon_db_${HOST}_${DB}_db_diff.log
     LOGF_DB_OLD=$LOGDIR/mon_db_${HOST}_${DB}_db_old.log
     touch $LOGF_DB_OLD
     LOGF_DB=$LOGDIR/mon_db_${HOST}_${DB}_db.log
-    cat $LOGF | egrep -A3 -i OPEN_MODE | grep -v '\----' | cut -c 1-108 | cut --complement -b '46-62' > $LOGF_DB
+    cat $LOGF | sed -n '1,3p' > $LOGF_DB
+    cat $LOGF | sed -n '4,6p' | cut -c 1-108 >> $LOGF_DB
     diff $LOGF_DB_OLD $LOGF_DB > $LOGF_DB_DIFF
-    rc_db=$?
 
-    if [ "$rc_inst" -ne 0 -a -s $LOGF_INST_DIFF ]; then
-      cat $LOGF_INST_DIFF $LOGF_INST_OLD $LOGF_INST | $WMMAIL -s "$MPREFIX INSTANCE status has been changed (host: ${HOST} / db: ${DB})" $ADMINS
+    if [ -s $LOGF_DB_DIFF ]; then
+	    ( cat $LOGF_DB_DIFF ; echo "Old_status:" ; cat $LOGF_DB_OLD ; echo "" ; echo  "Current_status:" ; cat $LOGF_DB ) | $WMMAIL -s "$MPREFIX DATABASE status has been changed: (host: ${HOST} / db: ${DB})" $ADMINS
     fi
-    if [ "$rc_db" -ne 0 -a -s $LOGF_DB_DIFF ]; then
-      cat $LOGF_DB_DIFF $LOGF_DB_OLD $LOGF_DB | $WMMAIL -s "$MPREFIX DATABASE status has been changed: (host: ${HOST} / db: ${DB})" $ADMINS
-    fi
-    cp $LOGF_INST $LOGF_INST_OLD
-#    cp $LOGF_DB_OLD $LOGF_DB_OLD"_old.log"
     cp $LOGF_DB $LOGF_DB_OLD
-#    rm $LOGF_INST_DIFF $LOGF_INST  $LOGF_DB_DIFF $LOGF_DB
+#    rm  $LOGF_DB_DIFF $LOGF_DB
   done
 done
 
