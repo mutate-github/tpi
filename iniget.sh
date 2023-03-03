01acdb0da24eedb4257c7a73ec8b7585811f8c9f
mutate-github <mutate@mail.ru>
2023-03-03 10:31:24 +0000

    last

--- iniget.sh
+++ iniget.sh
@@ -13,12 +13,6 @@
 # $ iniget file.ini Machine3
 # $ iniget file.ini Machine1 app
 
-# DBS
-aliases=/etc/profile.d/00-dbs-aliases.sh
-if [ -s $aliases ]; then
-  . /etc/profile.d/00-dbs-aliases.sh
-fi
-
 function iniget() {
   if [[ $# -lt 2 || ! -f $1 ]]; then
     echo "usage: iniget <file> [--list|<section> [key]]"
@@ -27,8 +21,7 @@ function iniget() {
   local inifile=$1
 
   if [ "$2" == "--list" ]; then
-#    for section in $(cat $inifile | sed '/ *#/d; /^ *$/d' | grep "\[" | sed -e "s#\[##g" | sed -e "s#\]##g"); do
-    for section in $(cat $inifile | sed '/^#.*$/d; /^ *$/d' | grep "\[" | sed -e "s#\[##g" | sed -e "s#\]##g"); do
+    for section in $(cat $inifile | sed '/ *#/d; /^ *$/d' | grep "\[" | sed -e "s#\[##g" | sed -e "s#\]##g"); do
       echo $section
     done
     return 0
@@ -40,31 +33,16 @@ function iniget() {
 
   # https://stackoverflow.com/questions/49399984/parsing-ini-file-in-bash
   # This awk line turns ini sections => [section-name]key=value
-  # local lines=$(awk '/\[/{prefix=$0; next} $1{print prefix $0}' $inifile)
-#  local lines=$(awk '/\[/{prefix=$0; gsub(/[ \t]+$/,"",prefix); next} $1{print prefix $0}' $inifile | sed '/ *#/d; /^ *$/d')
-  local lines=$(awk '/\[/{prefix=$0; gsub(/[ \t]+$/,"",prefix); next} $1{s=gensub("^#.*$","","G",$1); if(s!="") print prefix s}' $inifile)
+#  local lines=$(awk '/\[/{prefix=$0; next} $1{print prefix $0}' $inifile)
+  local lines=$(awk '/\[/{prefix=$0; gsub(/[ \t]+$/,"",prefix); next} $1{print prefix $0}' $inifile | sed '/ *#/d; /^ *$/d')
   for line in $lines; do
-    if [[ "$key" == "db" && -s $aliases ]]; then               # DBS
-       s1=$(grep $section /etc/profile.d/00-dbs-aliases.sh 2>/dev/null | sed 's/alias //' | awk -F= '{print $1}' | head -1)    #DBS
-       section=$s1                               #DBS
-    fi                                          #DBS
     if [[ "$line" == \[$section\]* ]]; then
       local keyval=$(echo $line | sed -e "s/^\[$section\]//")
       if [[ -z "$key" ]]; then
         echo $keyval
       else
         if [[ "$keyval" = $key=* ]]; then
-          if [[ "$section" =~ "servers"  ]]; then          #DBS
-              str1=$(echo $keyval | sed -e "s/^$key=//")     #DBS
-              str2=$(alias "$str1" 2>/dev/null|  awk -F"=" '{print $3}'  | tr -d \' | awk '{print $NF}')  #DBS
-              if [[ -n "$str2" ]]; then                    #DBS
-                echo $str2                                 #DBS
-              else
-                echo $(echo $keyval | sed -e "s/^$key=//")
-              fi
-          else                                             #DBS
-              echo $(echo $keyval | sed -e "s/^$key=//")
-          fi                                               #DBS
+          echo $(echo $keyval | sed -e "s/^$key=//")
         fi
       fi
     fi
@@ -73,11 +51,6 @@ function iniget() {
 
 
 BASEDIR=`dirname $0`
-INI_FILE="$BASEDIR/$1"
-if [ -f "$INI_FILE" ]; then
-  iniget $INI_FILE $2 $3
-else
-  echo "Configuration file: "$INI_FILE" not found. Exiting.."
-  exit 127
-fi
+
+iniget $BASEDIR/$1 $2 $3
 
