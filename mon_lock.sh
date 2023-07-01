f330573cbeb606c0952137a4e8cf91d5310adc16
mutate <mutate@mail.ru>
2023-07-01 21:01:51 +0600

    Sat Jul  1 21:01:51 +06 2023

--- mon_lock.sh
+++ mon_lock.sh
@@ -3,12 +3,12 @@ set -f
 
 BASEDIR=`dirname $0`
 LOGDIR="$BASEDIR/../log"
-MAILS=`$BASEDIR/iniget.sh mon.ini mail script`
-WMMAIL="$BASEDIR/$MAILS"
+#MAILS=`$BASEDIR/iniget.sh mon.ini mail script`
+#WMMAIL="$BASEDIR/$MAILS"
 WRTPI="$BASEDIR/rtpi"
-MPREFIX=`$BASEDIR/iniget.sh mon.ini mail prefix`
+#MPREFIX=`$BASEDIR/iniget.sh mon.ini mail prefix`
 HOSTS=`$BASEDIR/iniget.sh mon.ini servers host`
-ADMINS=`$BASEDIR/iniget.sh mon.ini admins email`
+#ADMINS=`$BASEDIR/iniget.sh mon.ini admins email`
 THRESHOLD=`$BASEDIR/iniget.sh mon.ini locks threshold`
 
 for HOST in `echo "$HOSTS" | xargs -n1 echo`; do
@@ -21,7 +21,8 @@ for HOST in `echo "$HOSTS" | xargs -n1 echo`; do
     CUR_VAL=`egrep "HOLDER|Waiter" $LOGF | sort -nk 11 | head -1 | awk -v lim=$THRESHOLD '{if($NF+0>=lim) {print $NF}}'`
 
     if [ -n "$CUR_VAL" ]; then
-       cat $LOGF | $WMMAIL -s "$MPREFIX Locks warning: ${HOST} / ${DB}  (current: $CUR_VAL min, threshold: $THRESHOLD min)" $ADMINS
+#       cat $LOGF | $WMMAIL -s "$MPREFIX Locks warning: ${HOST} / ${DB}  (current: $CUR_VAL min, threshold: $THRESHOLD min)" $ADMINS
+       cat $LOGF | $BASEDIR/send_msg.sh $HOST $DB "Locks warning: (current: $CUR_VAL min, threshold: $THRESHOLD min)"
        rm $LOGF 
     fi
   done # DB
