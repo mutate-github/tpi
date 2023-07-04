62f5c9fd6d3acc0e51b1e7c6c16094eaf738fb74
mutate <mutate@mail.ru>
2023-07-04 16:28:37 +0600

    Tue Jul  4 16:28:37 +06 2023

--- ttlgrm_bot.sh
+++ ttlgrm_bot.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+set -f
+
+# usage: echo "body message" | ttlgrm_bot.sh MSG_FRM_SRV1 HOST1 DB1
 
 MPREFIX=$1
 HOST=$2
 DB=$3
+shift
+shift
+shift
+ALL=$*
 
 msg=$(cat; echo x)
 msg=${msg%x}
@@ -10,6 +17,6 @@ msg=${msg%x}
 BASEDIR=`dirname $0`
 TLGRM_CMD=`$BASEDIR/iniget.sh mon.ini telegram cmd`
 TLGRM_CHT=`$BASEDIR/iniget.sh mon.ini telegram chat_id`
-$TLGRM_CMD mark0 $TLGRM_CHT "sender: ${MPREFIX} ${HOST}/${DB}
+$TLGRM_CMD mark0 $TLGRM_CHT "sender: \`${MPREFIX} ${HOST}/${DB} ${ALL}\`
 \`${msg} \`"
 
