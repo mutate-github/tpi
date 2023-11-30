b0c2d8b29779c821337b531a7b9d07980219834f
mutate-github <mutate@mail.ru>
2023-11-30 07:05:19 +0000

    Thu Nov 30 07:05:19 UTC 2023

--- diagram.sh
+++ diagram.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 set -f
 
-if [[ "$#" -lt 1 ]]; then
-  echo "usage: ./diagramm.sh sysmetric_h.log 25/11/23-0[89]  3 11 15 17 19 20 22 23 24"
-  echo "usage: tpi ... oratop h | diagramm.sh  3 11 15 17 19 20 22 23 24"
-  echo "          1           2     3      4    5    6    7      8      9    10    11    12    13    14    15    16     17       18       19     20      21     22         23      24      25     26     27"
-  echo " BEGIN_TIME        NCPU HCPUB CPUUPS LOAD DCTR DWTR   SPFR   TPGA   SCT   AAS   AST ASCPU  ASIO  ASWA  ASPQ   UTPS     UCPS     SSRT   MBPS    IOPS   IORL       LOGR    PHYR    PHYW   TEMP   DBTM"
-  echo ""
-  all_par="3 11 15 17 19 20 22 23 24" 
-else   
-  all_par="$*"
-fi
+echo "usage: ./diagramm.sh sysmetric_h.log 25/11/23-0[89]  3 11 15 17 19 20 22 23 24"
+echo "usage: tpi ... oratop h | diagramm.sh  3 11 15 17 19 20 22 23 24"
+echo "          1           2     3      4    5    6    7      8      9    10    11    12    13    14    15    16     17       18       19     20      21     22         23      24      25     26     27"
+echo " BEGIN_TIME        NCPU HCPUB CPUUPS LOAD DCTR DWTR   SPFR   TPGA   SCT   AAS   AST ASCPU  ASIO  ASWA  ASPQ   UTPS     UCPS     SSRT   MBPS    IOPS   IORL       LOGR    PHYR    PHYW   TEMP   DBTM"
+echo ""
+
+case "$#" in
+0|2) all_par="3 11 15 17 19 20 22 23 24" 
+   ;;
+*) all_par="$@"
+   ;;
+esac
 
 if [ -p /dev/stdin ]; then
   file="/tmp/$$_tmp.tmp"
@@ -22,6 +24,7 @@ else
   date="$2"
   shift
   shift
+  if [[ "$#" -ne 0 ]]; then  all_par="$@"; fi
   # col1="$3"
   # col2="$4"
 fi
@@ -33,7 +36,7 @@ scale=$(awk "BEGIN{print $limit/100}")
 
 tail -100 "${file}" | grep "BEGIN_TIME" | uniq > ${file}.tmp
 egrep "${date}" "${file}" | sort -n | uniq >> ${file}.tmp
-rm ${file}
+rm /tmp/$$_tmp.tmp 2>/dev/null
 file=${file}.tmp
 
 get_cols()
