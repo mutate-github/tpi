ab365f68ccfa56b3ca05f0988c055e2b75d0348a
mutate-github <mutate@mail.ru>
2023-12-28 10:28:28 +0000

    Thu Dec 28 10:28:28 UTC 2023

--- execsql.sh
+++ execsql.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$#" -lt 3 ]; then
   echo "Usage: $0 server database script.sql param1 param2 param3 ..."
+  echo "Usage: $0 server database dhashtop.sql dhashtop.sql username,sql_id session_type=\'FOREGROUND\' to_date\(\'27/12/23-09:40\',\'dd/mm/yy-hh24:mi\'\) to_date\(\'27/12/23-10:02\',\'dd/mm/yy-hh24:mi\'\)"
   exit
 fi 
 
@@ -12,7 +13,7 @@ shift
 shift
 shift
 PARAM="$@"
-PARAM=$(echo $PARAM | sed -e "s/'/\\\'/g")
+PARAM=$(echo $PARAM | sed -e "s/'/\\\'/g" -e 's|(|\\(|g' -e 's|)|\\)|g' )
 
 BASEDIR=`dirname $0`
 SET_ENV_F="$BASEDIR/set_env"
@@ -21,7 +22,7 @@ SET_ENV=`cat $SET_ENV_F`
 echo "SRV: $SRV     SID: $SID    SCRIPT: $SCRIPT     PARAM: $PARAM"
 
 cat <<EOFF > execsql_one_$$.sh
-#!/bin/sh
+#!/bin/bash
 sid=\$1
 $SET_ENV
 export ORACLE_SID=\$sid
