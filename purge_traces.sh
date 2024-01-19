97a5f5a2b20f84c7fea3dbc22d738c64f906684b
mutate-github <mutate@mail.ru>
2024-01-19 08:35:54 +0000

    Fri Jan 19 08:35:54 UTC 2024

--- purge_traces.sh
+++ purge_traces.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-
+#!/bin/bash
+set -f
 # ps -ef | awk -F_ '/[p]mon/{print $NF}' | while read i; do ./purge_traces.sh $i; done
 #
 # usage purge_traces.sh $ORACLE_SID
@@ -28,9 +28,7 @@ jet|ja)                     . /etc/profile.ora ;;
 aisutf*|unit*|crm)          . ~/.ora_env ;;
 askona*|aixtdb*|sbaskona)   . ~/.profile ;;
 egais*|GOLD506*)            . ~/.bashrc ;;
-goldwhs)                    . ~/.bash_profile ;;
 unc*)                       . ~/${sid}_setenv.sh ;;
-goldwhs)                    . ~/.bash_profile ;;
 # opndb)                      . ~/BRSKDB_setenv.sh ; login_str="tal/tal@'(DESCRIPTION=(ADDRESS=(COMMUNITY=TCP.WORLD)(PROTOCOL=TCP)(HOST=172.16.104.36)(PORT=1525))(CONNECT_DATA=(SERVICE_NAME=opndb)))'" ;;
 opndb*|obk*)                : ;;
 INSIS*|insis*|alfa|ALFA)    : ;;
@@ -50,28 +48,34 @@ INSIS*|insis*|alfa|ALFA)    : ;;
 ;;
 esac
 
+export ORACLE_SID=$sid
+
+VALUE=$(sqlplus -s '/as sysdba' <<'EOS'
+set lines 250  heading off feedback off pagesize 0 trimspool on timing off
+select value from v$system_parameter where name='diagnostic_dest';
+EOS
+)
+
+echo "diagnostic_dest: "$VALUE
+cd $VALUE
+
 trc=$(echo "show homes;"  | adrci | grep 'diag/rdbms/.*/'$sid'$')
 tns3=$(echo "show homes;"  | adrci | grep 'diag/tnslsnr/.*/')
+echo "trc: "$trc
+echo "tns3: "$tns3
 
 echo "set home for diag/rdbms traces: "$trc
 echo "set home for listener tns3: "$tns3
 echo "age: "$age
 
 for trc_ in $(echo $trc | xargs); do
-  echo "purge diag/rdbms/ ALERT TRACE INCIDENT: "$trc_
+  echo "purge diag/rdbms/ ALERT TRACE INCIDENT CDUMP: "$trc_
   adrci exec="set home $trc_ ; migrate schema"
   adrci exec="set home $trc_ ; purge -age $age -type ALERT"
   adrci exec="set home $trc_ ; purge -age $age -type TRACE"
   adrci exec="set home $trc_ ; purge -age $age -type INCIDENT"
+  adrci exec="set home $trc_ ; purge -age $age -type CDUMP"
 done
-
-VALUE=$(sqlplus -s '/as sysdba' <<'EOS'
-set lines 250  heading off feedback off pagesize 0 trimspool on
-select value from v$system_parameter where name='diagnostic_dest';
-EOS
-)
-
-echo "diagnostic_dest: "$VALUE
 
 for tns3_ in $(echo $tns3 | xargs); do
   echo "purge listener ALERT TRACE: "$tns3_
@@ -90,5 +94,7 @@ END
 )
 
 echo "purge audit logs for $VALUE :"
-find $VALUE  -type f -mtime +$audit -name "*.aud" | xargs -i -P20 rm {}
-find $VALUE  -type f -mtime +$audit -name "*.aud" -exec rm {} \;
+case $(uname | awk -F_ '{print $1}') in
+  Linux)   find $VALUE  -type f -mtime +$audit -name "*.aud" | xargs -i -P20 rm {} ;;
+  *)       find $VALUE  -type f -mtime +$audit -name "*.aud" -exec rm {} \; ;;
+esac
