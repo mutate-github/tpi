3c6e03cfcffaacb3cc6369e0b0848f84feea08e2
mutate-github <mutate@mail.ru>
2024-01-30 12:29:34 +0600

    tpi

--- update_git_tpi.sh
+++ update_git_tpi.sh
@@ -5,16 +5,21 @@
 #./tpi | sed -n '/Usage/,$p' > README.md
 #scp README.md 172.16.249.1:~/tal/
 
+if [ "$1" = "tpi" ]; then
 #askona
+echo "askona"
 scp tpi 192.168.1.53:tal/
 scp tpi 192.168.1.178:tal/
 
 #dixy
+echo "dixy"
 scp tpi 10.0.7.21:tal/
 scp tpi 10.0.17.21:tal/
 
 #as
+echo "as p260unc2"
 scp tpi p260unc2:tal/
+fi
 
 git add $*
 DT=`date`
