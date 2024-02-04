e42c58ac528f690f0a2c5b93facefc49c614f37a
oracle <oracle@mon.nomadlife.local>
2024-02-04 14:16:34 +0600

    nomad mon_all.sh

--- mon_all.sh
+++ mon_all.sh
@@ -9,38 +9,30 @@ echo $BASEDIR
 echo ""
 echo "START ALL MONITORING *****************************************************************************"`date`
 echo "Monitor: ================================================================================ mon_ping_ssh.sh"
-<<<<<<< HEAD
-$BASEDIR/mon_ping_ssh.sh
-echo "Monitor: ================================================================================ mon_diskspace.sh"
-$BASEDIR/mon_diskspace.sh
-#echo "Monitor: ================================================================================ mon_swap.sh"
-#$BASEDIR/mon_swap.sh
-=======
-$BASEDIR/mon_ping_ssh.sh 
+$BASEDIR/mon_ping_ssh.sh $CLIENT
 echo "Monitor: ================================================================================ mon_disksp.sh"
-$BASEDIR/mon_disksp.sh
+$BASEDIR/mon_disksp.sh $CLIENT
 echo "Monitor: ================================================================================ mon_swap.sh"
-$BASEDIR/mon_swap.sh
->>>>>>> aec192cb6d857577893ae93df0635f52c6e28ca6
+$BASEDIR/mon_swap.sh $CLIENT
 echo "Monitor: ================================================================================ mon_load.sh"
-$BASEDIR/mon_load.sh
+$BASEDIR/mon_load.sh $CLIENT
 echo "Monitor: ================================================================================ mon_db.sh"
 $BASEDIR/mon_db.sh $CLIENT
 echo "Monitor: ================================================================================ mon_alert.sh"
-$BASEDIR/mon_alert.sh
-#$BASEDIR/mon_adrcial.sh
+$BASEDIR/mon_alert.sh $CLIENT
+#$BASEDIR/mon_adrcial.sh $CLIENT
 echo "Monitor: ================================================================================ mon_fra.sh"
-$BASEDIR/mon_fra.sh
+$BASEDIR/mon_fra.sh $CLIENT
 echo "Monitor: ================================================================================ mon_tbs.sh"
-$BASEDIR/mon_tbs.sh
+$BASEDIR/mon_tbs.sh $CLIENT
 echo "Monitor: ================================================================================ mon_stb.sh"
-$BASEDIR/mon_stb.sh
+$BASEDIR/mon_stb.sh $CLIENT
 echo "Monitor: ================================================================================ mon_db_files.sh"
-$BASEDIR/mon_db_files.sh
+$BASEDIR/mon_db_files.sh $CLIENT
 echo "Monitor: ================================================================================ mon_limsess.sh"
-$BASEDIR/mon_limsess.sh
+$BASEDIR/mon_limsess.sh $CLIENT
 echo "Monitor: ================================================================================ mon_lock.sh"
-$BASEDIR/mon_lock.sh
+$BASEDIR/mon_lock.sh $CLIENT
 echo "FINISH ALL MONITORING ****************************************************************************"`date`
 
 
