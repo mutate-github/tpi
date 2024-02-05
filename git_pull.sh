437479f28f74d6d3c152986afec1cb5051748122
mutate-github <mutate@mail.ru>
2024-02-05 16:12:57 +0600

    git_pull.sh

---
+++ git_pull.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+git pull
+
+echo "oracle@amazon1"
+cat <<EOF | ssh oracle@a1 "/bin/bash"
+cd start/tpi
+git pull
+echo "scp to jet:"
+scp tpi jet:bin/
+
+echo "oracle@mon"
+cat <<EOL | ssh oracle@mon "/bin/bash"
+cd start/tpi
+git pull
+echo "scp to aisprod:"
+scp tpi aisprod:start/bin
+EOL
+EOF
+
+
