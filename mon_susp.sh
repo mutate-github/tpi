5ec94d819c03c6931a54e7cc417aa2749fbe93c0
mutate-github <mutate@mail.ru>
2024-11-04 15:14:13 +0000

    Mon Nov  4 15:14:13 UTC 2024

---
+++ mon_susp.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -f
+
+CLIENT="$1"
+BASEDIR=$(dirname $0)
+CONFIG="mon.ini"
+if [ -n "$CLIENT" ]; then
+  shift
+  CONFIG=${CONFIG}.${CLIENT}
+  if [ ! -s "$BASEDIR/$CONFIG" ]; then echo "Exiting... Config not found: "$CONFIG ; exit 128; fi
+fi
+echo "Using config: ${CONFIG}"
+
+LOGDIR="$BASEDIR/../log"
+if [ ! -d "$LOGDIR" ]; then mkdir -p "$LOGDIR"; fi
+WRTPI="$BASEDIR/rtpi"
+HOSTS=$($BASEDIR/iniget.sh $CONFIG servers host)
+
+THRESHOLD=$($BASEDIR/iniget.sh $CONFIG suspend threshold)
+
+for HOST in $(xargs -n1 echo <<< "$HOSTS"); do
+  echo "HOST="$HOST
+  DBS=$($BASEDIR/iniget.sh $CONFIG $HOST db)
+  for DB in $(xargs -n1 echo <<< "$DBS"); do
+    echo "DB="$DB
+    LOGF=$LOGDIR/mon_lock_${HOST}_${DB}.log
+    $WRTPI $HOST $DB lock  > $LOGF
+    CUR_VAL=$(egrep "Waiter" $LOGF | awk '{print $NF}' | sort -n | awk -v lim=$THRESHOLD '{if($NF+0>=lim) {print $NF}}' | head -1)
+    if [ -n "$CUR_VAL" ]; then
+       cat $LOGF | $BASEDIR/send_msg.sh $CONFIG $HOST $DB "Locks warning: (current: $CUR_VAL min, threshold: $THRESHOLD min)"
+       rm $LOGF 
+    fi
+  done # DB
+done # HOST
+
